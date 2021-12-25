26376b46416586c5e0bdc2fc29e508437c955518
uf-code <ugur.firat.developer@gmail.com>
2021-12-26 00:20:19 +0300

    adding bytes4 'z' 0x7a0000

--- solidity-andrei/07-fixed_size_arrays.sol
+++ solidity-andrei/07-fixed_size_arrays.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 contract FixedSizeArrays {
     uint256[3] public numbers = [3, 4, 7];
@@ -12,6 +12,8 @@ contract FixedSizeArrays {
     bytes2 public b2; // 0x0000
 
     bytes3 public b3; // 0x000000
+
+    bytes4 public b4; // 0x00000000
 
     // ... up to bytes32
 
@@ -27,5 +29,6 @@ contract FixedSizeArrays {
         b1 = "a";
         b2 = "ab";
         b3 = "abc";
+        b4 = "z";
     }
 }
