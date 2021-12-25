bcb934ca3f02c2ca2e79b3773cf31687ced926b6
uf-code <ugur.firat.developer@gmail.com>
2021-12-25 23:02:00 +0300

    bools and ints

---
+++ solidity-andrei/05-booleans_integers.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Property {
+    // Boolean
+    bool public sold;
+
+    // Integer
+    uint8 public x = 255; //max value
+    int8 public y = 127; // max value min value: -128
+}
