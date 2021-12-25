bed1fa33e5b64ef4e22f09ecfedcec482db5021d
uf-code <ugur.firat.developer@gmail.com>
2021-12-25 22:32:27 +0300

    constructor, addresses, and immutable variables

---
+++ solidity-andrei/04_contructor.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Property {
+    int256 public price;
+    string public location;
+    address public owner;
+
+    constructor(int256 _price, string memory _location) {
+        price = _price;
+        location = _location;
+        owner = msg.sender;
+    }
+
+    function setPrice(int256 _price) public {
+        price = _price;
+    }
+
+    function setLocation(string memory _location) public {
+        location = _location;
+    }
+
+    function setOwner(address _owner) public {
+        owner = _owner;
+    }
+}
