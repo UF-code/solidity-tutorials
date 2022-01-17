61a79d48ff7fa728586122a9c78d3ef04f8ef1f1
uf-code <ugur.firat.developer@gmail.com>
2022-01-17 03:15:49 +0300

    Storage vs Memory

--- solidity-andrei/11_Storage_vs_Memory.sol
+++ solidity-andrei/11_Storage_vs_Memory.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.0 <0.9.0;
 contract A{
     string[] public cities = ['Prague', 'Bucharest'];
 
-    function f_memory() public{
+    function f_memory() public view{
         string[] memory s1 = cities;
         s1[0] = 'Tokyo';
     }
