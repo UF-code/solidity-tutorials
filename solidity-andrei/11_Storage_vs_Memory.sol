dc99dbe66ed3521c945f7745887d2ce0c90e8bd0
uf-code <ugur.firat.developer@gmail.com>
2022-01-17 03:15:09 +0300

    Storage vs Memory

---
+++ solidity-andrei/11_Storage_vs_Memory.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.0 <0.9.0;
+
+contract A{
+    string[] public cities = ['Prague', 'Bucharest'];
+
+    function f_memory() public{
+        string[] memory s1 = cities;
+        s1[0] = 'Tokyo';
+    }
+    function f_storate() public{
+        string[] storage s1 = cities;
+        s1[0] = 'Berlin';
+    }
+}
