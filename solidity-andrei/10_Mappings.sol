efd899897c372c1097a180d8d5bcad1b6a7c976b
uf-code <ugur.firat.developer@gmail.com>
2022-01-17 03:04:01 +0300

    Mappings

---
+++ solidity-andrei/10_Mappings.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+contract Auction{
+    mapping(address => uint) public bids; 
+
+    function bid() payable public{
+        bids[msg.sender] = msg.value;
+    }
+}
