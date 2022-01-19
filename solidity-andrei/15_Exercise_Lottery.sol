b2586c3472768fb2d92b4db4db48600a14584fd2
uf-code <ugur.firat.developer@gmail.com>
2022-01-19 09:27:13 +0300

    Receive Function

--- solidity-andrei/15_Exercise_Lottery.sol
+++ solidity-andrei/15_Exercise_Lottery.sol
@@ -10,4 +10,17 @@ contract Lottery{
         manager = msg.sender;
     }
 
+
+    // Contract can have only one receive function
+    // Contract receive's eth from Externally Owned Account(EOA)
+    // Function can not take arguments neither return them
+    // Visibility must be external 
+    // Must be payable
+    receive() external payable{
+        players.push(payable(msg.sender));
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
 }
