cdb3d46cdc62f271e5667440654c5b4703ad4e86
uf-code <ugur.firat.developer@gmail.com>
2022-01-20 11:21:59 +0300

    Auction Initialized

---
+++ solidity-andrei/16_Auction.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.0 <0.9.0;
+
+contract Auction{
+    address payable public owner;
+    uint public startBlock;
+    uint public endBlock;
+    string public ipfsHash;
+
+    enum State {Started, Running, Ended, Canceled}
+    State public auctionState;
+
+    uint public highestBindingBid;
+    address payable public highestBidder;
+
+    mapping(address => uint) public bids;
+    uint bidIncrement;
+
+    constructor(){
+        owner = payable(msg.sender);
+        auctionState = State.Running;
+        startBlock = block.number;
+        endBlock = startBlock + 40320;
+        ipfsHash = "";
+        bidIncrement = 100;
+    }
+
+
+
+
+}
