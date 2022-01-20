6030dd4306c59f17fb117d83d8212cf91cd7f036
uf-code <ugur.firat.developer@gmail.com>
2022-01-20 11:26:26 +0300

    Function Modifiers

---
+++ solidity-andrei/17_Function_Modifiers.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.0 <0.9.0;
+
+contract Property{
+    uint public price;
+    address public owner;
+
+    // declaring the constructor
+    constructor(){
+        price = 0;
+        owner = msg.sender;
+    }
+
+    function changeOwner(address _owner) public{
+        owner = _owner;
+    }
+
+    function setPrice(uint _price) public{
+        price = _price;
+    }
+}
