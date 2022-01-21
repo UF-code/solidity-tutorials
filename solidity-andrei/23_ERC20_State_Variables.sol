6c5c54ccad33281345320ce8f68296f5b58696a7
uf-code <ugur.firat.developer@gmail.com>
2022-01-21 03:46:34 +0300

    Balance and Transfer Functionality Has Been Added

--- solidity-andrei/23_ERC20_State_Variables.sol
+++ solidity-andrei/23_ERC20_State_Variables.sol
@@ -9,12 +9,12 @@ interface ERC20Interface{
     function balanceOf(address tokenOwner) external view returns (uint balance);
     function transfer(address to, uint tokens) external returns (bool success);
 
-    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-    function approve(address spender, uint tokens) external returns (bool success);
-    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+    // function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    // function approve(address spender, uint tokens) external returns (bool success);
+    // function transferFrom(address from, address to, uint tokens) external returns (bool success);
 
     event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    // event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 
 
 }
@@ -30,4 +30,26 @@ contract Cryptos is ERC20Interface{
     address public founder;
     mapping(address => uint) public balances;
     // balances[0x11111....] = 100;
+
+    constructor(){
+        totalSupply = 1000000;
+        founder = msg.sender;
+        balances[founder] = totalSupply;
+    }
+
+    function balanceOf(address tokenOwner) public view override returns (uint balance){
+        return balances[tokenOwner];
+    }
+
+    function transfer(address to, uint tokens) public override returns(bool success){
+        require(balances[msg.sender] >= tokens);
+
+        balances[to] += tokens;
+        balances[msg.sender] -= tokens;
+        emit Transfer(msg.sender, to, tokens);
+
+        return true;
+    }
+
+
 }
