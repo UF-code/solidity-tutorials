4bbdda74f7969a1918e3f67d81faf3945ae1998e
uf-code <ugur.firat.developer@gmail.com>
2022-01-21 03:00:03 +0300

    Inheritance in Solidity

---
+++ solidity-andrei/20_Inheritance.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.0 <0.9.0;
+
+contract BaseContract{
+    int public x;
+    address public owner;
+
+    constructor(){
+        x = 5;
+        owner = msg.sender;
+    }
+
+    function setX(int _x) public{
+        x = _x;
+    }
+
+}
+
+contract A is BaseContract{
+    int public y = 3;
+    
+}
