ed1d4d9db8b851a696825900fdeb47b83d7e100c
uf-code <ugur.firat.developer@gmail.com>
2022-03-08 00:43:59 +0300

    adding few changes

--- solidity-north/01-solidity_basics/01-setter_getter.sol
+++ solidity-north/01-solidity_basics/01-setter_getter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 
-contract Property {
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SetterandGetter {
     int256 public price;
 
     function setPrice(int256 _price) public {
