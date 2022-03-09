6f16f2aad90ffd7fccadfac5282c1e439fbb0ec4
uf-code <ugur.firat.developer@gmail.com>
2022-03-09 21:22:21 +0300

    creating a deployer

--- solidity-grider/inbox/deploy.js
+++ solidity-grider/inbox/deploy.js
@@ -1,1 +1,8 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const { interface, bytecode } = require('./compile.js')
+require('dotenv').config()
+
+const provider = new HDWalletProvider(process.env.wallet, process.env.network)
+
+const web3 = new Web3(provider)
