130d2a488b2ffcabc977e4631355ca21fbfd1e32
uf-code <ugur.firat.developer@gmail.com>
2022-03-10 11:10:52 +0300

    enter functionality :D:D:D

--- solidity-grider/02-lottery/lottery.sol
+++ solidity-grider/02-lottery/lottery.sol
@@ -2,9 +2,16 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
+    address payable[] public players;
+
+    // mapping(address => uint256) player;
 
     // Constructor
     function Lottery() public {
         manager = msg.sender;
+    }
+
+    function enter() public payable {
+        players.push(msg.sender);
     }
 }
