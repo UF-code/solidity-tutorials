913a152e5e72e60c2c6dfcfd07565635e7a18995
uf-code <ugur.firat.developer@gmail.com>
2022-03-12 05:42:55 +0300

    creating campaign smart contract

---
+++ solidity-grider/03-campaign/campaign.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.4.17;
+
+contract Campaign {
+    address public manager;
+    uint256 public minimumContribution;
+
+    function Campaign(uint256 minimum) public {
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+}
