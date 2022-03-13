b0706aacbc71e04ca109ef323d9ce69126f1d1b3
uf-code <ugur.firat.developer@gmail.com>
2022-03-13 11:53:23 +0300

    campaign test

--- solidity-grider/03-campaign/kickstart/ethereum/compile.js
+++ solidity-grider/03-campaign/kickstart/ethereum/compile.js
@@ -12,8 +12,8 @@ const output = solc.compile(source, 1).contracts
 fs.ensureDirSync(buildPath)
 
 for (let contract in output) {
-  fs.outputJSONSync(
-    path.resolve(buildPath, contract.replace(':', '') + '.json'),
-    output[contract]
-  )
+    fs.outputJSONSync(
+        path.resolve(buildPath, contract.replace(':', '') + '.json'),
+        output[contract]
+    )
 }
