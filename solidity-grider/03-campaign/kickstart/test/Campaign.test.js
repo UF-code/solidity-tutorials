99364668fb92714d079fe7d98ce6ec587667a7fa
uf-code <ugur.firat.developer@gmail.com>
2022-03-13 12:54:16 +0300

    last test

--- solidity-grider/03-campaign/kickstart/test/Campaign.test.js
+++ solidity-grider/03-campaign/kickstart/test/Campaign.test.js
@@ -80,4 +80,33 @@ describe('Campaign', () => {
 
         assert.equal('Buy batteries', request.description)
     })
+
+    it('processes requests', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei('10', 'ether'),
+        })
+
+        await campaign.methods
+            .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+            .send({ from: accounts[0], gas: '1000000' })
+
+        await campaign.methods
+            .approveRequest(0)
+            .send({ from: accounts[0], gas: '1000000' })
+
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gas: '1000000',
+        })
+
+        let balance = await web3.eth.getBalance(accounts[1])
+        console.log(balance)
+        balance = web3.utils.fromWei(balance, 'ether')
+        console.log(balance)
+        balance = parseFloat(balance)
+        console.log(balance)
+
+        assert(balance > 104)
+    })
 })
